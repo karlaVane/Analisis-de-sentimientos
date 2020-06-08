22c7ca3516e3f369e738c57d6b6b834e03297d21
seguvega <seguvega@hotmail.com>
2020-06-08 00:59:10 -0500

    Correccion pregunta 3 para Web

--- pregunta3.py
+++ pregunta3.py
@@ -3,12 +3,7 @@ from generarTweets import generarTweet
 import csv
 import unidecode
 import re
-consulta="coronavirus"    #la consulta que se realizara a la API de tweeter
-cantidadTweets = 100    #La cantidad de tweets que se consultaran
-date_since = "2020-05-29"   #fecha hasta
-date_until = "2020-06-03"   #fecha desde, OJO--> coge tweets de un dia menos
-nombre_archivo = "Tweets con textBlob"
-datos = generarTweet(consulta, cantidadTweets, date_since, date_until)
+
 def analisisSentimientos(corpus):
     analisis = TextBlob(corpus)
     if analisis.sentiment.polarity > 0:
@@ -18,19 +13,26 @@ def analisisSentimientos(corpus):
     else:
         return ("Negativo",-1)
 
+def crearArchivo(nombre_archivo,sentimiento,fecha,autor,texto):
+    crearArch = open("%s.csv" % nombre_archivo, "w", newline='',
+                     encoding="utf-8")  # Nombre del nuevo archivo si se quiere sobre escribir poner el missmo!
+    writer = csv.writer(crearArch)
+    writer.writerow(["FECHA", "AUTOR", "TEXTO", "ANALISIS DE SENTIMIENTO TBLOB"])
+    for col in range(len(sentimiento)):
+        writer.writerow([fecha[col], autor[col], texto[col], sentimiento[col]])
+    print("Archivo %s.csv Generado !! :)" % nombre_archivo)
 
-fecha, autor, texto, sentimiento = ([] for i in range(4))
-print("[------------------TEXTO------------------, -----------------SENTIMIENTO----------------]")
-for columna in datos:
-    fecha.append(columna[0])
-    autor.append(columna[1])
-    texto.append(columna[2])
-    sentimiento.append(analisisSentimientos(columna[2]))
-    print([columna[2], analisisSentimientos(columna[2])])
 
-crearArch = open("%s.csv" %nombre_archivo, "w", newline='', encoding="utf-8")   #Nombre del nuevo archivo si se quiere sobre escribir poner el missmo!
-writer = csv.writer(crearArch)
-writer.writerow(["FECHA", "AUTOR", "TEXTO", "ANALISIS DE SENTIMIENTO TBLOB"])
-for col in range(len(sentimiento)):
-    writer.writerow([fecha[col], autor[col], texto[col], sentimiento[col]])
-print("Archivo %s.csv Generado !! :)" %nombre_archivo)
+def resultadoSPregunta3(consulta, cantidadTweets, date_since, date_until):
+    datos = generarTweet(consulta, cantidadTweets, date_since, date_until)
+
+    fecha, autor, texto, sentimiento = ([] for i in range(4))
+    print("[------------------TEXTO------------------, -----------------SENTIMIENTO----------------]")
+    for columna in datos:
+        fecha.append(columna[0])
+        autor.append(columna[1])
+        texto.append(columna[2])
+        sentimiento.append(analisisSentimientos(columna[2]))
+        print([columna[2], analisisSentimientos(columna[2])])
+    return (fecha, autor, texto, sentimiento)
+
